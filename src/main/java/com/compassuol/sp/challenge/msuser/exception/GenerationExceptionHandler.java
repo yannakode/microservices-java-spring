28da937fa28458e503916e13a32acd11942c34d2
yannakode <yanann1997@gmail.com>
2023-11-09 14:22:44 -0300

    Implement authentication with JWT token and login method, handle login error

--- src/main/java/com/compassuol/sp/challenge/msuser/exception/GenerationExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.compassuol.sp.challenge.msuser.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class GenerationExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
