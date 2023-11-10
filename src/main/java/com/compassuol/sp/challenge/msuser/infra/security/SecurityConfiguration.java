2a0d4ffe41756c5bde0f158c60092154d6ac5788
yannakode <yanann1997@gmail.com>
2023-11-10 20:42:43 -0300

    fixed error login

--- src/main/java/com/compassuol/sp/challenge/msuser/infra/security/SecurityConfiguration.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/infra/security/SecurityConfiguration.java
@@ -1,24 +1,50 @@
 package com.compassuol.sp.challenge.msuser.infra.security;
 
+import com.compassuol.sp.challenge.msuser.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     @Autowired
     SecurityFilter securityFilter;
+    @Autowired
+    private UserRepository userRepository;
+    private Optional<UserDetails> findUserDetails(String userEmail){
+        return Optional.of(userRepository.findByEmail(userEmail));
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> findUserDetails(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
